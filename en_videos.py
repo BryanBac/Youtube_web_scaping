2345eca10f720b06a5e8c4cad718b96b50250f88
BryanBac <49624724+BryanBac@users.noreply.github.com>
2022-08-23 18:39:49 -0600

    Implementación de API
    Se implementó la api para obtener estadisticas de los videos.
    Se reemplazó el Webscraping anterior debido a falta de alcance en la obtención de datos

--- en_videos.py
+++ en_videos.py
@@ -1,21 +1,66 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
+#  Función para estadisticas
+def get_channel_stats(youtube, channel_ids):
+    request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        id=",".join(channel_ids)
+    )
+    all_data = []
+    response = request.execute()
+    for i in range(len(response["items"])):
+        data = dict(Channel_name=response["items"][i]["snippet"]["title"],
+                    Subscribers=response["items"][i]["statistics"]["subscriberCount"],
+                    Views=response["items"][i]["statistics"]["viewCount"],
+                    Total_videos=response["items"][i]["statistics"]["videoCount"],
+                    playlist_id=response["items"][i]["contentDetails"]["relatedPlaylists"]["uploads"]
+                    )
+        all_data.append(data)
+    return all_data
 
 
-links = [
-    "https://www.youtube.com/watch?v=P-bBKa8Bb_Q"
-]
+def get_videos_ids(youtube, playlist_id):
+    request = youtube.playlistItems().list(
+        part="contentDetails",
+        playlistId=playlist_id,
+        maxResults=50
+    )
+    response = request.execute()
+    netx_page_token = response.get("nextPageToken")
+    more_pages = True
+    videos_ides = []
+    for i in range(len(response["items"])):
+        videos_ides.append(response["items"][i]["contentDetails"]["videoId"])
+    while more_pages:
+        if netx_page_token is None:
+            more_pages = False
+        else:
+            request = youtube.playlistItems().list(
+                part="contentDetails",
+                playlistId=playlist_id,
+                maxResults=50,
+                pageToken=netx_page_token
+            )
+            response = request.execute()
+            for i in range(len(response["items"])):
+                videos_ides.append(response["items"][i]["contentDetails"]["videoId"])
+            netx_page_token = response.get("nextPageToken")
+    return videos_ides
 
 
-def obtner():
-    print("me faltan los likes")
-    driver = webdriver.Chrome("chromedriver_win32\\chromedriver.exe")
-    driver.get(links[0])
-    content = driver.page_source.encode("utf-8").strip()
-    soup = BeautifulSoup(content, 'lxml')
-    duración = soup.findAll("span", class_="ytp-time-duration")
-    print(duración[0].text)
-    pruebas = soup.findAll("div", class_="top-level-buttons style-scope ytd-menu-renderer")
-    for prueba in pruebas:
-        print(prueba, "-")
-        #  UN NO FUNCIONA
+def get_videos_details(youtube, video_ids):
+    all_video_stats = []
+    for i in range(0, len(video_ids), 50):
+        request = youtube.videos().list(
+            part="snippet,statistics",
+            id=",".join(video_ids[i:i+50]))
+        response = request.execute()
+        for video in response["items"]:
+            video_stats = dict(
+                Title=video["snippet"]["title"],
+                Published_date=video["snippet"]["publishedAt"],
+                Views=video["statistics"]["viewCount"],
+                Likes=video["statistics"]["likeCount"],
+                Total_fav=video["statistics"]["favoriteCount"],
+                #  Total_comentarios=video["statistics"]["commentCount"]
+            )
+            all_video_stats.append(video_stats)
+    return all_video_stats
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-from en_videos import obtner
+from en_videos import get_channel_stats, get_videos_ids, get_videos_details
+from googleapiclient.discovery import build
+import pprint
 
 
 urls = [
@@ -8,6 +10,15 @@ urls = [
 ]
 
 
+api_key = "AIzaSyAas7rC594WDvAwKaXpFgaTCv_-mbTJAUo"
+channel_ids = ["UC3n5uGu18FoCy23ggWWp8tA",
+               "UCoSrY_IQQVpmIRZ9Xf-y93g"
+               ]
+
+youtube = build("youtube", "v3", developerKey=api_key)
+
+
+#  Ya no se usa el main
 def main():
     driver = webdriver.Chrome("chromedriver_win32\\chromedriver.exe")
     driver.get("https://www.youtube.com/channel/{}/videos".format(urls[0]))
@@ -28,4 +39,11 @@ def main():
 
 
 #  main()
-obtner()
+all_data = get_channel_stats(youtube, channel_ids)
+for i in range(len(all_data)):
+    pprint.pprint(all_data[i])
+    videos = get_videos_ids(youtube, all_data[i]["playlist_id"])
+    pprint.pprint(get_videos_details(youtube, videos))
+    #  print(get_videos_details(youtube, videos))  #  -- Para el json
+    print("\n\n\n----")
+#  aquí arriba ando imprimiendo el diccionario de datos
